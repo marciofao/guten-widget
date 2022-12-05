92570c78e7676704cf383648465b37c5acb1786e
Marcio Fão <marcio.lopes.fao@gmail.com>
2022-12-05 09:16:34 -0300

    setup node

--- guten-widget.php
+++ guten-widget.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: Guten Widget
 */
@@ -6,7 +7,13 @@ Plugin Name: Guten Widget
 function myguten_enqueue() {
     wp_enqueue_script(
         'myguten-script',
-        plugins_url( 'scripts.js', __FILE__ )
+        plugins_url( 'scripts.js', __FILE__ ),
+        array( 'wp-blocks' )
     );
 }
 add_action( 'enqueue_block_editor_assets', 'myguten_enqueue' );
+
+function myguten_stylesheet() {
+    wp_enqueue_style( 'myguten-style', plugins_url( 'style.css', __FILE__ ) );
+}
+add_action( 'enqueue_block_assets', 'myguten_stylesheet' );
