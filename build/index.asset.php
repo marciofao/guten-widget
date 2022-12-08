6f82173bc1acd25a582c82065cd725f423d05ec6
Marcio Fão <marcio.lopes.fao@gmail.com>
2022-12-08 15:04:11 -0300

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element'), 'version' => '4733311d92119a8ca19c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element'), 'version' => '35175f9eb5eaa6eb6e8b');
