4b767ac066f6df94651ba87f379a148ae20209b4
Marcio Fão <marcio.lopes.fao@gmail.com>
2022-12-13 10:53:28 -0300

    Add plugin author details

--- guten-widget.php
+++ guten-widget.php
@@ -1,13 +1,25 @@
 <?php
 
-//ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
-
 /*
-Plugin Name: Guten Widget
+ * Plugin Name:       Guten Widget
+ * Description:       Creates "Resort Now" block in Gutenberg editor
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           1.0.0
+ * Author:            Márcio Lopes Fão
+ * Author URI:        https://www.linkedin.com/in/m%C3%A1rcio-lopes-f%C3%A3o-bb559133
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       gutenpride
 */
 
 require_once('FnuggHandler.php');
 require_once('resort_now_render.php');
+
+
+/* 
+ * Setup Block editor
+ */ 
 
 function resort_now_block() {
     register_block_type( __DIR__ );
@@ -25,9 +37,10 @@ wp_register_script(
 );
 
 /*
+ * Initializes the API
  * usage: http://localhost/multivision/wp-json/wp/v2/guten-widget?s=fonna
  */
-function gw_setup() {
+function rnb_api_setup() {
 
     register_rest_route('wp/v2', '/guten-widget', array(
         'methods' => 'GET',
@@ -35,21 +48,20 @@ function gw_setup() {
     ));
 
 }
-add_action( 'init', 'gw_setup' );
+add_action( 'init', 'rnb_api_setup' );
+
+/* 
+ * handle the API 
+ */ 
 
 function fnugg_handler($data) {
-
    $res = new FnuggHandler($data);
    return $res->handle_request();
 }
 
-function dump_die($a){
-    echo "<pre>";
-    var_dump($a);
-    die;
-}
-
-// register custom meta tag field
+/* 
+ * register custom meta tag field
+ */ 
 function myguten_register_post_meta() {
     register_post_meta( 'post', 'resort_now', array(
         'show_in_rest' => true,
@@ -60,7 +72,11 @@ function myguten_register_post_meta() {
 add_action( 'init', 'myguten_register_post_meta' );
 
 
-function myguten_render_paragraph( $block_attributes, $content ) {
+/* 
+ * Renderring the block on frontend
+ */
+
+function resort_now_call( $block_attributes, $content ) {
     $value = get_post_meta( get_the_ID(), 'resort_now', true );
     // check value is set before outputting
     if ( $value ) {
@@ -72,23 +88,17 @@ function myguten_render_paragraph( $block_attributes, $content ) {
 
 register_block_type( 'core/resort-now', array(
     'api_version' => 2,
-    'render_callback' => 'myguten_render_paragraph',
+    'render_callback' => 'resort_now_call',
 ) );
 
 
-add_action('init', 'rnb_style');
+/* 
+ * enquequee js and styling on frontend
+ */
+function rnb_scripts() {
 
-function rnb_style()
-{
-    
-}
-
-function rnb_scripts($hook) {
-
-	
 	wp_enqueue_script( 'custom_js', plugins_url( 'rnb-fetch.js', __FILE__ ), array(), rand(0,99999) );
 	wp_enqueue_style('rnb-style', plugins_url( 'style.css', __FILE__ ), array(), rand(0,99999));
-  
 
 }
 add_action('wp_enqueue_scripts', 'rnb_scripts');
